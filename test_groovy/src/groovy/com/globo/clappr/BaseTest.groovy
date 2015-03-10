3c8eca1dd2a58cbd54a44a77e1b47ac01dc57a22
Bruno Torres <bruno.torres@me.com>
2015-03-10 11:37:26 -0300

    finish porting existing tests to groovy

--- test_groovy/src/groovy/com/globo/clappr/BaseTest.groovy
+++ test_groovy/src/groovy/com/globo/clappr/BaseTest.groovy
@@ -1,9 +1,9 @@
 package com.globo.clappr
 
-import android.content.Context
 import com.globo.clappr.components.PlayerInfo
 import groovy.transform.CompileStatic
-import org.junit.BeforeClass
+import org.junit.After
+import org.junit.Before
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
@@ -11,4 +11,10 @@ import org.robolectric.RobolectricTestRunner
 @CompileStatic
 @RunWith(RobolectricTestRunner)
 abstract class BaseTest {
+    @Before void setUp() {
+        PlayerInfo.setContext(Robolectric.application)
+    }
+
+    @After void tearDown() {
+    }
 }
--- test_groovy/src/groovy/com/globo/clappr/base/BaseObjectTest.groovy
+++ test_groovy/src/groovy/com/globo/clappr/base/BaseObjectTest.groovy
@@ -1,88 +1,131 @@
 package com.globo.clappr.base
 
-import android.content.Intent
 import com.globo.clappr.BaseTest
-import com.globo.clappr.base.BaseObject.EventHandler
 import groovy.transform.CompileStatic
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.robolectric.annotation.Config
 
+import static org.hamcrest.CoreMatchers.is
 import static org.junit.Assert.assertThat
-import static org.hamcrest.CoreMatchers.*
-import static org.junit.matchers.JUnitMatchers.*
 
 @CompileStatic
-@Config(manifest=Config.NONE)
+@Config(manifest = Config.NONE)
 class BaseObjectTest extends BaseTest {
-  BaseObject testObj
+    BaseObject testObj
 
-  @BeforeClass static void setUpClass() {
-    PlayerInfo.setContext(Robolectric.application)
-  }
+    @Before
+    void setUp() {
+        super.setUp()
+        testObj = new BaseObject()
+    }
 
-  @Before
-  void setUp() {
-    testObj = new BaseObject()
-  }
+    @After
+    void tearDown() {
+        super.tearDown()
+        testObj.stopListening()
+    }
 
-  @After
-  void tearDown() {
-    testObj.stopListening()
-  }
+    @Test
+    void onCallbackShouldBeCalledOnTrigger() {
+        def callbackCalled = false
+        testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
+        testObj.trigger("baseobject:testevent", true)
+        assertThat callbackCalled, is(true)
+    }
 
-  @Test
-  void onCallbackShouldBeCalledOnTrigger() {
-    def callbackCalled = false
-    testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
-    testObj.trigger("baseobject:testevent", true)
-    assertThat callbackCalled, is(true)
-  }
+    @Test
+    void onCallbackShouldNotBeCalledForAnotherEventName() {
+        def callbackCalled = false
+        testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
+        testObj.trigger("baseobject:testevent2", true)
+        assertThat callbackCalled, is(false)
+    }
 
-  @Test
-  void onCallbackShouldNotBeCalledForAnotherEventName() {
-    def callbackCalled = false
-    testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
-    testObj.trigger("baseobject:testevent2", true)
-    assertThat callbackCalled, is(false)
-  }
+    @Test
+    void onCallbackShouldNotBeCalledForAnotherContextObject() {
+        def triggerObj = new BaseObject()
+        def callbackCalled = false
+        testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
+        triggerObj.trigger("baseobject:testevent", true)
+        assertThat callbackCalled, is(false)
+    }
 
-  @Test
-  void onCallbackShouldNotBeCalledForAnotherContextObject() {
-    def triggerObj = new BaseObject()
-    def callbackCalled = false
-    testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
-    triggerObj.trigger("baseobject:testevent", true)
-    assertThat callbackCalled, is(false)
-  }
+    @Test
+    void onCallbackShouldNotBeCalledWhenHandlerIsRemoved() {
+        def callbackCalled = false
+        BaseObject.EventHandler eventHandler = { intent -> callbackCalled = true }
+        testObj.on("baseobject:testevent", eventHandler)
+        testObj.off("baseobject:testevent", eventHandler)
+        testObj.trigger("baseobject:testevent", true)
+        assertThat callbackCalled, is(false)
+    }
 
-  @Test
-  void onCallbackShouldNotBeCalledWhenHandlerIsRemoved() {
-    def callbackCalled = false
-    def eventHandler = { intent -> println('teste'); callbackCalled = true }
-    testObj.on("baseobject:testevent", eventHandler)
-    testObj.off("baseobject:testevent", eventHandler)
-    testObj.trigger("baseobject:testevent", true)
-    // assertThat callbackCalled, is(false)
-    assert callbackCalled == false
-  }
+    @Test
+    void onceCallbackShouldBeCalledOnEvent() {
+        def callbackCalled = false
+        testObj.once("baseobject:testevent", { intent -> callbackCalled = true })
+        testObj.trigger("baseobject:testevent", true)
+        assertThat callbackCalled, is(true)
+    }
 
-  @Test
-  void onceCallbackShouldBeCalledOnEvent() {
-    def callbackCalled = false
-    testObj.once("baseobject:testevent", { intent -> callbackCalled = true })
-    testObj.trigger("baseobject:testevent", true)
-    assertThat callbackCalled, is(true)
-  }
+    @Test
+    void onceCallbackShouldNotBeCalledTwice() {
+        def callbackCalled = false
+        testObj.once("baseobject:testevent", { intent -> callbackCalled = true })
+        testObj.trigger("baseobject:testevent", true)
+        callbackCalled = false
+        testObj.trigger("baseobject:testevent", true)
+        assertThat callbackCalled, is(false)
+    }
 
-  @Test
-  void onceCallbackShouldNotBeCalledTwice() {
-    def callbackCalled = false
-    testObj.once("baseobject:testevent", { intent -> callbackCalled = true })
-    testObj.trigger("baseobject:testevent", true)
-    callbackCalled = false
-    testObj.trigger("baseobject:testevent", true)
-    assertThat callbackCalled, is(false)
-  }
+    @Test
+    void stopListeningShouldCancelAllEventHandlers() {
+        def callbackCalled = false
+        def callbackCalled2 = false
+        testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
+        testObj.on("baseobject:testevent2", { intent -> callbackCalled2 = true })
+        testObj.stopListening()
+        testObj.trigger("baseobject:testevent", true)
+        testObj.trigger("baseobject:testevent2", true)
+        assertThat callbackCalled, is(false)
+        assertThat callbackCalled2, is(false)
+    }
+
+    @Test
+    void stopListeningShouldCancelEventHandlersOnlyOnContextObject() {
+        def testObj2 = new BaseObject()
+        def callbackCalled = false
+        def callbackCalled2 = false
+        testObj.on("baseobject:testevent", { intent -> callbackCalled = true })
+        testObj2.on("baseobject:testevent2", { intent -> callbackCalled2 = true })
+        testObj.stopListening()
+        testObj.trigger("baseobject:testevent", true)
+        testObj2.trigger("baseobject:testevent2", true)
+        assertThat callbackCalled, is(false)
+        assertThat callbackCalled2, is(true)
+    }
+
+    @Test
+    void listenToShouldFireCallbackForAnEventOnAGivenContextObject() {
+        def contextObj = new BaseObject()
+        def callbackCalled = false
+        testObj.listenTo(contextObj, "baseobject:testevent", { intent -> callbackCalled = true })
+        contextObj.trigger("baseobject:testevent", true)
+        assertThat callbackCalled, is(true)
+    }
+
+
+    @Test
+    void stopListeningShouldCancelHandlerForAnEventOnAGivenContextObject() {
+        def testObj = new BaseObject()
+        def contextObj = new BaseObject()
+        def callbackCalled = false
+        BaseObject.EventHandler handler = { intent -> callbackCalled = true }
+        testObj.listenTo(contextObj, "baseobject:testevent", handler)
+        testObj.stopListening(contextObj, "baseobject:testevent", handler)
+        contextObj.trigger("baseobject:testevent", true)
+        assertThat callbackCalled, is(false)
+    }
 }
