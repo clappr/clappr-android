b1270a345274da14d5d4d38c04c0cb65d23c91c0
Bruno Torres <me@brunotorr.es>
2015-10-16 14:16:54 -0300

    add Container/Playback classes

---
+++ player/src/main/java/com/globo/clappr/components/Container.kt
@@ -0,0 +1,9 @@
+package com.globo.clappr.components
+
+import com.globo.clappr.base.UIObject
+
+public open class Container: UIObject {
+    constructor(options: Map<String, Object>?) : super(options) {
+    }
+
+}
---
+++ player/src/main/java/com/globo/clappr/components/Playback.kt
@@ -0,0 +1,8 @@
+package com.globo.clappr.components
+
+import com.globo.clappr.base.UIObject
+
+public open class Playback : UIObject {
+    constructor(options: Map<String, Object>?): super(options) {
+    }
+}
