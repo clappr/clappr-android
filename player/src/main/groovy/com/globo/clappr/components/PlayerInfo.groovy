88b547ca506ba66018a2544032e8d3771eef54a5
Fernando Pinho <fpinho@gmail.com>
2016-04-12 15:08:05 -0300

    remove legacy references

--- player/src/main/groovy/com/globo/clappr/components/PlayerInfo.groovy
+++
@@ -1,9 +0,0 @@
-package com.globo.clappr.components
-
-import android.content.Context
-import groovy.transform.CompileStatic
-
-@CompileStatic
-class PlayerInfo {
-    static Context context = null
-}
