8fa19ea0ecce61aa34d280b2355460e925142381
Fernando Pinho <fpinho@gmail.com>
2016-10-05 18:14:58 -0300

    feat(base): update base component’s constructors

--- player/src/main/kotlin/com/globo/clappr/components/Container.kt
+++ player/src/main/kotlin/com/globo/clappr/components/Container.kt
@@ -1,11 +1,9 @@
 package com.globo.clappr.components
 
+import com.globo.clappr.plugin.Loader
 import com.globo.clappr.base.Options
 import com.globo.clappr.base.UIObject
 
-open class Container: UIObject {
-    constructor(options: Options?) : super() {
-    }
-
+open class Container(options: Options) : UIObject() {
     var playback: Playback? = null
 }
--- player/src/main/kotlin/com/globo/clappr/components/Playback.kt
+++ player/src/main/kotlin/com/globo/clappr/components/Playback.kt
@@ -1,9 +1,8 @@
 package com.globo.clappr.components
 
+import com.globo.clappr.plugin.Loader
 import com.globo.clappr.base.Options
 import com.globo.clappr.base.UIObject
 
-open class Playback : UIObject {
-    constructor(options: Options?): super() {
-    }
+open class Playback(options: Options) : UIObject() {
 }
