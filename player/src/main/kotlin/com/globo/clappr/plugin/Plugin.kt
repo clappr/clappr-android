b96bb69ba562fe93e414ec756192bbfaaa8f25c4
Fernando Pinho <fpinho@gmail.com>
2016-10-05 18:15:30 -0300

    feat(base): update initial Plugin state

--- player/src/main/kotlin/com/globo/clappr/plugin/Plugin.kt
+++ player/src/main/kotlin/com/globo/clappr/plugin/Plugin.kt
@@ -2,7 +2,7 @@ package com.globo.clappr.plugin
 
 import com.globo.clappr.base.BaseObject
 
-class Plugin : BaseObject(), PluginInterface {
+open class Plugin : BaseObject(), PluginInterface {
     override val name = "plugin"
-    var state = PluginState.ENABLED
+    var state = PluginState.DISABLED
 }
