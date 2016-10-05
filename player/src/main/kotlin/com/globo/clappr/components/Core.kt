b8cf939c9ba9103ed36e72c70e7641ca41c6bdd3
Fernando Pinho <fpinho@gmail.com>
2016-10-05 18:14:58 -0300

    feat(base): update base component’s constructors

--- player/src/main/kotlin/com/globo/clappr/components/Core.kt
+++ player/src/main/kotlin/com/globo/clappr/components/Core.kt
@@ -1,12 +1,10 @@
 package com.globo.clappr.components
 
+import com.globo.clappr.plugin.Loader
 import com.globo.clappr.base.Options
 import com.globo.clappr.base.UIObject
 
-open class Core: UIObject {
-    constructor(options: Options?) : super() {
-    }
-
+open class Core(val loader: Loader, options: Options) : UIObject() {
     var activeContainer: Container? = null
     var containers: MutableMap<String, Container> = hashMapOf()
 }
