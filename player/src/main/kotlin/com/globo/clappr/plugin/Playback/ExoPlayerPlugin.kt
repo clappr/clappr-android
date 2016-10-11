ecfbdb4bd13f914c68586349f4c41b9a60605960
Diego Marcon <diego.marcon@corp.globo.com>
2016-10-11 17:01:12 -0300

    feat(exoplayer): add dummy playback with sample video

--- player/src/main/kotlin/com/globo/clappr/plugin/Playback/ExoPlayerPlugin.kt
+++ player/src/main/kotlin/com/globo/clappr/plugin/Playback/ExoPlayerPlugin.kt
@@ -1,11 +1,20 @@
 package com.globo.clappr.plugin.Playback
 
 import android.graphics.Color
-import android.util.Log
+import android.net.Uri
+import android.os.Handler
 import android.view.ViewGroup
-import com.globo.clappr.base.BaseObject
 import com.globo.clappr.components.Playback
 import com.globo.clappr.plugin.UIPlugin
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 
 open class ExoPlayerPlugin(val playback: Playback) : UIPlugin(playback) {
     companion object {
@@ -14,8 +23,26 @@ open class ExoPlayerPlugin(val playback: Playback) : UIPlugin(playback) {
         var containerView: ViewGroup? = null
     }
 
+    var playerView = SimpleExoPlayerView(context)
+
     init {
-        this.playback.view?.setBackgroundColor(Color.parseColor("#00ff00"))
-        containerView?.addView(this.playback.view)
+        playerView.setBackgroundColor(Color.parseColor("#00ff00"))
+        containerView?.addView(playerView)
+
+        val mainHandler = Handler()
+        val bandwidthMeter = DefaultBandwidthMeter()
+        val videoTrackSelectionFactory = AdaptiveVideoTrackSelection.Factory(bandwidthMeter)
+        val trackSelector = DefaultTrackSelector(mainHandler, videoTrackSelectionFactory)
+
+        val player = ExoPlayerFactory.newSimpleInstance(context, trackSelector, DefaultLoadControl())
+        playerView.player = player
+        player.playWhenReady = true
+
+        val dataSourceFactory = DefaultDataSourceFactory(context, "agent", bandwidthMeter)
+        val extractorsFactory = DefaultExtractorsFactory()
+        val mp4VideoUri = Uri.parse("http://clappr.io/highline.mp4")
+
+        val videoSource = ExtractorMediaSource(mp4VideoUri, dataSourceFactory, extractorsFactory, null, null)
+        player.prepare(videoSource)
     }
 }
