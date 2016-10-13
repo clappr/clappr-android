d1c0ead168a0fad0a973b3925579878cb4138b56
Diego Marcon <diego.marcon@corp.globo.com>
2016-10-13 17:19:10 -0300

    feat(exoplayer): add basic hls playback for testing

--- player/src/main/kotlin/com/globo/clappr/plugin/Playback/ExoPlayerPlugin.kt
+++ player/src/main/kotlin/com/globo/clappr/plugin/Playback/ExoPlayerPlugin.kt
@@ -3,18 +3,24 @@ package com.globo.clappr.plugin.Playback
 import android.graphics.Color
 import android.net.Uri
 import android.os.Handler
+import android.util.Log
 import android.view.ViewGroup
 import com.globo.clappr.components.Playback
 import com.globo.clappr.plugin.UIPlugin
 import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Format
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.AdaptiveMediaSourceEventListener
 import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DataSpec
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import java.io.IOException
 
 open class ExoPlayerPlugin(val playback: Playback) : UIPlugin(playback) {
     companion object {
@@ -24,6 +30,8 @@ open class ExoPlayerPlugin(val playback: Playback) : UIPlugin(playback) {
     }
 
     var playerView = SimpleExoPlayerView(context)
+    val hlsLogger = HlsLogger()
+    val urlString = "URL STRING"
 
     init {
         playerView.setBackgroundColor(Color.parseColor("#00ff00"))
@@ -39,10 +47,35 @@ open class ExoPlayerPlugin(val playback: Playback) : UIPlugin(playback) {
         player.playWhenReady = true
 
         val dataSourceFactory = DefaultDataSourceFactory(context, "agent", bandwidthMeter)
-        val extractorsFactory = DefaultExtractorsFactory()
-        val mp4VideoUri = Uri.parse("http://clappr.io/highline.mp4")
+        val mp4VideoUri = Uri.parse(urlString)
 
-        val videoSource = ExtractorMediaSource(mp4VideoUri, dataSourceFactory, extractorsFactory, null, null)
-        player.prepare(videoSource)
+        val source = HlsMediaSource(mp4VideoUri, dataSourceFactory, mainHandler, hlsLogger)
+        player.prepare(source)
+    }
+
+    class HlsLogger(): AdaptiveMediaSourceEventListener {
+        override fun onLoadStarted(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long) {
+            Log.i("EXOPlayer", "onLoadStarted")
+        }
+
+        override fun onDownstreamFormatChanged(trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaTimeMs: Long) {
+            Log.i("EXOPlayer", "onDownstreamFormatChanged")
+        }
+
+        override fun onUpstreamDiscarded(trackType: Int, mediaStartTimeMs: Long, mediaEndTimeMs: Long) {
+            Log.i("EXOPlayer", "onUpstreamDiscarded")
+        }
+
+        override fun onLoadCanceled(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long, loadDurationMs: Long, bytesLoaded: Long) {
+            Log.i("EXOPlayer", "onLoadCanceled")
+        }
+
+        override fun onLoadCompleted(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long, loadDurationMs: Long, bytesLoaded: Long) {
+            Log.i("EXOPlayer", "onLoadCompleted")
+        }
+
+        override fun onLoadError(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long, loadDurationMs: Long, bytesLoaded: Long, error: IOException?, wasCanceled: Boolean) {
+            Log.i("EXOPlayer", "onLoadError")
+        }
     }
 }
