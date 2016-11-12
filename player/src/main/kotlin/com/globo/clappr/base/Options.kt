fe1601c3681b8bad450ab1757f9d5be483a62cf0
Bruno Torres <me@brunotorr.es>
2016-11-12 02:55:00 -0200

    feat(base): add typed parameters to Options class

--- player/src/main/kotlin/com/globo/clappr/base/Options.kt
+++ player/src/main/kotlin/com/globo/clappr/base/Options.kt
@@ -1,5 +1,14 @@
 package com.globo.clappr.base
 
-class Options {
-    val options = mutableMapOf<String, Any>()
+import com.globo.clappr.components.Playback
+import com.globo.clappr.plugin.Plugin
+import kotlin.reflect.KClass
+
+class Options(
+    var source: String? = null,
+    var mimeType: String? = null,
+    var autoPlay: Boolean = true,
+    var plugins: List<out KClass<Plugin>> = emptyList(),
+    var playbacks: List<out KClass<Playback>> = emptyList(),
+    val options: Map<String, Any> = mutableMapOf<String, Any>()): Map<String, Any> by options {
 }
