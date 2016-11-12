71b37563c1827dccee753c923c361ad6e4121a39
Bruno Torres <me@brunotorr.es>
2016-11-12 02:53:23 -0200

    feat(base): add source/playback loading logic to container

--- player/src/main/kotlin/com/globo/clappr/components/Container.kt
+++ player/src/main/kotlin/com/globo/clappr/components/Container.kt
@@ -3,6 +3,7 @@ package com.globo.clappr.components
 import com.globo.clappr.plugin.Loader
 import com.globo.clappr.base.Options
 import com.globo.clappr.base.UIObject
+import com.globo.clappr.playback.NoOpPlayback
 import com.globo.clappr.plugin.Plugin
 
 open class Container(val loader: Loader, val options: Options) : UIObject() {
@@ -12,6 +13,22 @@ open class Container(val loader: Loader, val options: Options) : UIObject() {
 
     init {
         plugins = loader.loadPlugins(this)
+        val source = options.source
+        val mimeType = options.mimeType
+        if (source != null) {
+            load(source, mimeType)
+        }
     }
 
+    fun load(source: String, mimeType: String? = null): Boolean {
+        var supported = playback?.load(source, mimeType) ?: false
+        if (!supported) {
+            playback = loader.loadPlayback(source, mimeType, options)
+            supported = playback?.name == NoOpPlayback.name ?: false
+        }
+        return supported
+    }
 }
+
+// Base - Core, Lifecycle, Playback, etc - 11
+// MediaControl - Plugins, Render, etc - 16
--- player/src/test/kotlin/com/globo/clappr/com/globo/clappr/components/ContainerTest.kt
+++ player/src/test/kotlin/com/globo/clappr/com/globo/clappr/components/ContainerTest.kt
@@ -2,7 +2,10 @@ package com.globo.clappr.base
 
 import com.globo.clappr.BuildConfig
 import com.globo.clappr.components.Container
-import com.globo.clappr.plugin.Container.ContainerPlugin
+import com.globo.clappr.components.Playback
+import com.globo.clappr.components.PlaybackSupportInterface
+import com.globo.clappr.playback.NoOpPlayback
+import com.globo.clappr.plugin.container.ContainerPlugin
 import com.globo.clappr.plugin.Loader
 import org.junit.*
 import org.junit.Assert.*
@@ -14,9 +17,23 @@ import org.robolectric.shadows.ShadowApplication
 @RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class, sdk = intArrayOf(23))
 open class ContainerTest {
+
+    class MP4Playback(source: String, mimeType: String?, options: Options) : Playback(source, mimeType, options) {
+        companion object: PlaybackSupportInterface {
+            override fun supportsSource(source: String, mimeType: String?): Boolean {
+                return source.endsWith(".mp4")
+            }
+
+            override val name: String?
+                get() = "mp4"
+        }
+    }
+
     @Before
     fun setup() {
         BaseObject.context = ShadowApplication.getInstance().applicationContext
+        Loader.clearPlugins()
+        Loader.clearPlaybacks()
     }
 
     @Test
@@ -25,5 +42,24 @@ open class ContainerTest {
         val container = Container(Loader(), Options())
 
         assertTrue("no plugins", container.plugins.isNotEmpty())
+    }
+
+    @Test
+    fun shouldLoadPlaybackForSupportedSource() {
+        Loader.registerPlayback(MP4Playback::class)
+        val container = Container(Loader(), Options("some_source.mp4"))
+
+        assertNotNull("should have created playback", container.playback)
+        assertEquals("should have created mp4 playback", container.playback?.name, MP4Playback.name)
+    }
+
+    @Test
+    fun shouldLoadNoOpPlaybackForUnsupportedSource() {
+        Loader.registerPlayback(NoOpPlayback::class)
+        Loader.registerPlayback(MP4Playback::class)
+        val container = Container(Loader(), Options("some_unknown_source.mp0"))
+
+        assertNotNull("should have created playback", container.playback)
+        assertEquals("should have created no-op playback", container.playback?.name, NoOpPlayback.name)
     }
 }
