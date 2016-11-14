0f0736d93e0a0932f008e2a388ffc4d46857ede5
Bruno Torres <bruno.torres@me.com>
2016-11-14 00:19:50 -0200

    refactor(base): change no-op name property to plugin derived class standard

--- player/src/main/kotlin/com/globo/clappr/playback/NoOpPlayback.kt
+++ player/src/main/kotlin/com/globo/clappr/playback/NoOpPlayback.kt
@@ -6,11 +6,10 @@ import com.globo.clappr.components.PlaybackSupportInterface
 
 open class NoOpPlayback(source: String, mimeType: String? = null, options: Options = Options()) : Playback(source, mimeType, options) {
     companion object: PlaybackSupportInterface {
+        override val name: String = "no_op"
+
         override fun supportsSource(source: String, mimeType: String?): Boolean {
             return true
         }
-
-        override val name: String?
-            get() = "no_op"
     }
 }
