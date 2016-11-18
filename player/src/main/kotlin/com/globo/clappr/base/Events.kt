8dc9dafe3b4e6f869ed12231a19279e12b13078c
Fernando Pinho <fpinho@gmail.com>
2016-11-18 01:23:25 -0200

    feat(base): add playback events

--- player/src/main/kotlin/com/globo/clappr/base/Events.kt
+++ player/src/main/kotlin/com/globo/clappr/base/Events.kt
@@ -2,5 +2,14 @@ package com.globo.clappr.base
 
 enum class Event(val value: String) {
     READY("ready"),
-    ERROR("error")
+    ERROR("error"),
+    WILL_PLAY("willPlay"),
+    PLAYING("playing"),
+    WILL_PAUSE("willPause"),
+    DID_PAUSE("didPause"),
+    WILL_SEEK("willSeek"),
+    WILL_STOP("willStop"),
+    DID_STOP("didStop"),
+    DID_COMPLETE("didComplete"),
+    STALLED("stalled")
 }
