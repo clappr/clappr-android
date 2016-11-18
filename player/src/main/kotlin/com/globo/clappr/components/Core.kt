aa1ca3e41a2348de8d2a941e2dc32ebae7b79696
Fernando Pinho <fpinho@gmail.com>
2016-11-18 01:15:51 -0200

    feat(base): add active container and playback events

--- player/src/main/kotlin/com/globo/clappr/components/Core.kt
+++ player/src/main/kotlin/com/globo/clappr/components/Core.kt
@@ -1,6 +1,9 @@
 package com.globo.clappr.components
 
+import android.os.Bundle
 import android.widget.FrameLayout
+import com.globo.clappr.base.Callback
+import com.globo.clappr.base.InternalEvent
 import com.globo.clappr.plugin.Loader
 import com.globo.clappr.base.Options
 import com.globo.clappr.base.UIObject
@@ -9,8 +12,19 @@ import com.globo.clappr.plugin.Plugin
 class Core(val loader: Loader, val options: Options) : UIObject() {
     val plugins: List<Plugin>
 
-    var activeContainer: Container? = null
     val containers: MutableList<Container> = mutableListOf()
+    var activeContainer: Container? = null
+        set(value) {
+            activeContainer?.stopListening()
+            field = value
+            activeContainer?.on(InternalEvent.PLAYBACK_CHANGED.value,
+                    Callback.wrap { bundle: Bundle? -> trigger(InternalEvent.ACTIVE_PLAYBACK_CHANGED.value, bundle) }
+            )
+            trigger(InternalEvent.ACTIVE_CONTAINER_CHANGED.value)
+        }
+
+    val activePlayback: Playback?
+        get() = activeContainer?.playback
 
     val frameLayout: FrameLayout
         get() = view as FrameLayout
