40feaed79341cb7b7527006352b0f2fcc5c5214f
Diego Marcon <diego.marcon@corp.globo.com>
2016-11-22 10:21:57 -0200

    refactor(events): remove unused events

--- player/src/main/kotlin/com/globo/clappr/base/Events.kt
+++ player/src/main/kotlin/com/globo/clappr/base/Events.kt
@@ -3,11 +3,6 @@ package com.globo.clappr.base
 enum class Event(val value: String) {
     READY("ready"),
     ERROR("error"),
-    IDLE("idle"),
-    PLAY("play"),
-    STOP("stop"),
-    SEEK("seek"),
-    PAUSE("pause"),
     WILL_PLAY("willPlay"),
     PLAYING("playing"),
     WILL_PAUSE("willPause"),
