4a712d3ec709ea9be855c904334ab844931c0ea5
Fernando Pinho <fpinho@gmail.com>
2016-11-23 19:30:08 -0200

    feat(mediaplayer): add test with scheduler

--- player/src/test/kotlin/com/globo/clappr/playback/MediaPlayerPlaybackTest.kt
+++ player/src/test/kotlin/com/globo/clappr/playback/MediaPlayerPlaybackTest.kt
@@ -26,18 +26,30 @@ import org.robolectric.shadows.ShadowMediaPlayer
 import org.robolectric.shadows.ShadowSurfaceView
 import org.robolectric.shadows.util.DataSource
 import org.robolectric.util.ActivityController
+import org.robolectric.util.Scheduler
 import java.io.IOException
 
 @RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class, sdk = intArrayOf(23))
 open class MediaPlayerPlaybackTest {
     lateinit var mediaPlayerPlayback : MediaPlayerPlayback
+    lateinit var scheduler: Scheduler
+    lateinit var validMedia: ShadowMediaPlayer.MediaInfo
 
     @Before
     fun setup() {
         BaseObject.context = ShadowApplication.getInstance().applicationContext
 
-        ShadowMediaPlayer.addMediaInfo(DataSource.toDataSource("valid"), ShadowMediaPlayer.MediaInfo())
+        ShadowMediaPlayer.setCreateListener { mediaPlayer, shadowMediaPlayer ->
+            // Disabling invalid state emulation as it is not compatible with releases newer than KitKat (MediaTimeProvider)
+            shadowMediaPlayer.invalidStateBehavior = ShadowMediaPlayer.InvalidStateBehavior.SILENT
+        }
+
+        scheduler = Robolectric.getForegroundThreadScheduler()
+
+        validMedia = ShadowMediaPlayer.MediaInfo(1000,0)
+        ShadowMediaPlayer.addMediaInfo(DataSource.toDataSource("valid"), validMedia)
+
         mediaPlayerPlayback = MediaPlayerPlayback(source = "valid")
         createSurface(mediaPlayerPlayback)
         mediaPlayerPlayback.render()
@@ -72,13 +84,37 @@ open class MediaPlayerPlaybackTest {
     }
 
     @Test
+    fun shouldOnlyPlayOnValidState() {
+        val mpp : MediaPlayerPlayback = MediaPlayerPlayback(source = "valid")
+
+        var callbackCalled = false
+        mpp.on(Event.WILL_PLAY.value, Callback.wrap { bundle: Bundle? -> callbackCalled = true })
+        mpp.on(Event.PLAYING.value, Callback.wrap { bundle: Bundle? -> callbackCalled = true })
+
+        var result = mpp.play()
+        assertFalse("play allowed in invalid state", result)
+        assertFalse("play callback called in invalid state", callbackCalled)
+
+        result = mediaPlayerPlayback.play()
+        assertTrue("play not allowed in valid state", result)
+    }
+
+    @Test
+    fun shouldOnlyAllowPlayInValidState() {
+        val mpp : MediaPlayerPlayback = MediaPlayerPlayback(source = "valid")
+
+        assertFalse("play allowed in invalid state", mpp.canPlay)
+        assertTrue("play not allowed in valid state", mediaPlayerPlayback.canPlay)
+    }
+
+    @Test
     fun shouldTransitionToPlayingWhenPlay() {
         mediaPlayerPlayback.play()
 
         assertEquals("should transition to PLAYING", Playback.State.PLAYING, mediaPlayerPlayback.state)
     }
 
-    @Test()
+    @Test
     fun shouldTriggerPlayEventsWhenPlay() {
         var willPlayCount = 0
         var playingCount = 0
@@ -92,5 +128,79 @@ open class MediaPlayerPlaybackTest {
 
         assertEquals("will play triggered", 1, willPlayCount)
         assertEquals("playing not triggered", 1, playingCount)
+    }
+
+    @Test
+    fun shouldTriggerEventOnCompletion() {
+        var callbackCalled = false
+        mediaPlayerPlayback.on(Event.DID_COMPLETE.value, Callback.wrap { bundle: Bundle? -> callbackCalled = true })
+
+        mediaPlayerPlayback.play()
+        assertFalse("complete called", callbackCalled)
+        assertEquals("not playing", Playback.State.PLAYING, mediaPlayerPlayback.state)
+
+        scheduler.advanceBy(50)
+        assertFalse("complete called", callbackCalled)
+        assertEquals("not playing", Playback.State.PLAYING, mediaPlayerPlayback.state)
+
+        scheduler.advanceBy(950)
+        assertTrue("complete not called", callbackCalled)
+    }
+
+    @Test
+    fun shouldStopAllInteractionsOnError() {
+        var callbackCalled = false
+        mediaPlayerPlayback.on(Event.ERROR.value, Callback.wrap { bundle: Bundle? -> callbackCalled = true })
+
+        validMedia.scheduleErrorAtOffset(400, -1, -2)
+
+        mediaPlayerPlayback.play()
+        assertFalse("error callback called", callbackCalled)
+        assertTrue("play not allowed", mediaPlayerPlayback.canPlay)
+        assertTrue("stop not allowed", mediaPlayerPlayback.canStop)
+        assertTrue("seek not allowed", mediaPlayerPlayback.canSeek)
+        assertTrue("pause not allowed", mediaPlayerPlayback.canPause)
+
+        scheduler.advanceBy(200)
+        assertFalse("error callback called", callbackCalled)
+        assertTrue("play not allowed", mediaPlayerPlayback.canPlay)
+        assertTrue("stop not allowed", mediaPlayerPlayback.canStop)
+        assertTrue("seek not allowed", mediaPlayerPlayback.canSeek)
+        assertTrue("pause not allowed", mediaPlayerPlayback.canPause)
+
+        scheduler.advanceBy(200)
+        assertTrue("error callback not called", callbackCalled)
+        assertFalse("play allowed", mediaPlayerPlayback.canPlay)
+        assertFalse("play method allowed", mediaPlayerPlayback.play())
+        assertFalse("stop allowed", mediaPlayerPlayback.canStop)
+        assertFalse("stop method allowed", mediaPlayerPlayback.stop())
+        assertFalse("seek allowed", mediaPlayerPlayback.canSeek)
+        assertFalse("seek method allowed", mediaPlayerPlayback.seek(0))
+        assertFalse("pause allowed", mediaPlayerPlayback.canPause)
+        assertFalse("pause method allowed", mediaPlayerPlayback.pause())
+    }
+
+    @Test
+    fun shouldTriggerStalledEvents() {
+        var stalledCallbackCalled = false
+        mediaPlayerPlayback.on(Event.STALLED.value, Callback.wrap { bundle: Bundle? -> stalledCallbackCalled = true })
+        var playingCallbackCalled = false
+        mediaPlayerPlayback.on(Event.PLAYING.value, Callback.wrap { bundle: Bundle? -> playingCallbackCalled = true })
+
+        validMedia.scheduleBufferUnderrunAtOffset(150, 100)
+
+        mediaPlayerPlayback.play()
+        assertFalse("stalled callback called", stalledCallbackCalled)
+
+        scheduler.advanceBy(100)
+        assertFalse("stalled callback called", stalledCallbackCalled)
+
+        playingCallbackCalled = false
+        scheduler.advanceBy(50)
+        assertTrue("stalled callback not called", stalledCallbackCalled)
+        assertFalse("playing callback called", playingCallbackCalled)
+
+        scheduler.advanceBy(100)
+        assertTrue("playing callback not called", playingCallbackCalled)
     }
 }
