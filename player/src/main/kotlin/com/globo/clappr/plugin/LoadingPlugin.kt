19c3d789f9c8b4caba47054b9327a7a5f5544c96
Augusto Boranga <augusto.boranga@corp.globo.com>
2016-11-25 10:34:59 -0200

    feature(loading_plugin): add ProgressBar var

--- player/src/main/kotlin/com/globo/clappr/plugin/LoadingPlugin.kt
+++ player/src/main/kotlin/com/globo/clappr/plugin/LoadingPlugin.kt
@@ -1,12 +1,20 @@
 package com.globo.clappr.plugin
 
 import android.content.Context
+import android.widget.ProgressBar
 import com.globo.clappr.base.NamedType
 import com.globo.clappr.components.Container
 import com.globo.clappr.plugin.container.UIContainerPlugin
 
 open class LoadingPlugin(container: Container, context: Context) : UIContainerPlugin(container) {
+
+    private var spinner: ProgressBar?
+
     companion object: NamedType {
         override val name = "spinner"
+    }
+
+    init {
+        spinner = ProgressBar(context)
     }
 }
