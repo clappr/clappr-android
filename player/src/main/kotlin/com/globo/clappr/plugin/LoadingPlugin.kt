5dcaf61848d7b37f49dc80ca96c760fb735b37d7
Augusto Boranga <augusto.boranga@corp.globo.com>
2016-11-25 10:46:40 -0200

    feature(loading_plugin): bind events to their respective callbacks

--- player/src/main/kotlin/com/globo/clappr/plugin/LoadingPlugin.kt
+++ player/src/main/kotlin/com/globo/clappr/plugin/LoadingPlugin.kt
@@ -1,7 +1,10 @@
 package com.globo.clappr.plugin
 
 import android.content.Context
+import android.view.View
 import android.widget.ProgressBar
+import com.globo.clappr.base.Callback
+import com.globo.clappr.base.Event
 import com.globo.clappr.base.NamedType
 import com.globo.clappr.components.Container
 import com.globo.clappr.plugin.container.UIContainerPlugin
@@ -16,5 +19,20 @@ open class LoadingPlugin(container: Container, context: Context) : UIContainerPl
 
     init {
         spinner = ProgressBar(context)
+        bindEventListeners()
+    }
+
+    fun bindEventListeners() {
+        listenTo(this, Event.READY.value, startAnimating)
+        listenTo(this, Event.PLAYING.value, stopAnimating)
+        listenTo(this, Event.DID_COMPLETE.value, stopAnimating)
+    }
+
+    private val startAnimating = Callback.wrap {
+        spinner!!.visibility = View.VISIBLE
+    }
+
+    private val stopAnimating = Callback.wrap {
+        spinner!!.visibility = View.INVISIBLE
     }
 }
