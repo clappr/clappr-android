dca78aa78b3c9494d10f9b969b2e48caf5b562f2
UelitonFreitas <freitas.ueliton@gmail.com>
2016-11-28 16:57:16 -0200

    refactor(loading_plugin): divide bindEventListeners to separated methods

--- player/src/main/kotlin/com/globo/clappr/plugin/LoadingPlugin.kt
+++ player/src/main/kotlin/com/globo/clappr/plugin/LoadingPlugin.kt
@@ -14,6 +14,7 @@ import android.widget.ProgressBar
 import com.globo.clappr.base.Callback
 import com.globo.clappr.base.Event
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> feature(loading_plugin): create LoadingPlugin class
 =======
@@ -21,6 +22,9 @@ import android.widget.ProgressBar
 >>>>>>> feature(loading_plugin): add ProgressBar var
 =======
 >>>>>>> feature(loading_plugin): bind events to their respective callbacks
+=======
+import com.globo.clappr.base.InternalEvent
+>>>>>>> refactor(loading_plugin): divide bindEventListeners to separated methods
 import com.globo.clappr.base.NamedType
 import com.globo.clappr.components.Container
 import com.globo.clappr.plugin.container.UIContainerPlugin
@@ -74,28 +78,37 @@ open class LoadingPlugin(container: Container) : UIContainerPlugin(container) {
     init {
         setupSpinner()
         bindEventListeners()
-
     }
 
     private fun setupSpinner() {
         spinner = ProgressBar(context)
         spinner?.visibility = View.INVISIBLE
         container.frameLayout.addView(spinner)
-
     }
 
     fun bindEventListeners() {
-        listenTo(container.playback!!, Event.READY.value, startAnimating)
-        listenTo(container.playback!!, Event.PLAYING.value, stopAnimating)
-        listenTo(container.playback!!, Event.DID_COMPLETE.value, stopAnimating)
+        container.on(InternalEvent.DID_CHANGE_PLAYBACK.value, bindLoadingVisibilityCallBacks())
     }
 
-    private val startAnimating = Callback.wrap {
-        spinner?.visibility = View.VISIBLE
+    private fun bindLoadingVisibilityCallBacks(): Callback {
+        return Callback.wrap {
+            container.playback?.on(Event.STALLED.value, startAnimating())
+            container.playback?.on(Event.PLAYING.value, stopAnimating())
+        }
     }
 
-    private val stopAnimating = Callback.wrap {
-        spinner?.visibility = View.INVISIBLE
+    private fun startAnimating(): Callback {
+        return Callback.wrap {
+            spinner?.visibility = View.VISIBLE
+            visibility = Visibility.VISIBLE
+        }
+    }
+
+    private fun stopAnimating(): Callback {
+        return Callback.wrap {
+            spinner?.visibility = View.INVISIBLE
+            visibility = Visibility.HIDDEN
+        }
     }
 >>>>>>> feature(loading_plugin): add ProgressBar var
 }
