233c056ee92dc72b2856241c77fdb70231b2a64e
Bruno Torres <me@brunotorr.es>
2016-12-01 00:12:23 -0200

    refactor(base): change Core/Container to render plugins on their own render call

--- player/src/main/kotlin/io/clappr/player/components/Container.kt
+++ player/src/main/kotlin/io/clappr/player/components/Container.kt
@@ -8,6 +8,7 @@ import io.clappr.player.base.Options
 import io.clappr.player.base.UIObject
 import io.clappr.player.playback.NoOpPlayback
 import io.clappr.player.plugin.Plugin
+import io.clappr.player.plugin.container.UIContainerPlugin
 
 class Container(val loader: Loader, val options: Options) : UIObject() {
     val plugins: List<Plugin>
@@ -46,25 +47,22 @@ class Container(val loader: Loader, val options: Options) : UIObject() {
                 playback = null
             }
             supported = playback != null
+            render()
         }
         return supported
     }
 
     override fun render(): Container {
+        frameLayout.removeAllViews()
         val playback = this.playback
         playback?.let {
             frameLayout.addView(playback.view)
-            sendPlayBackViewToBack(playback)
             playback.render()
         }
+        plugins.filterIsInstance(UIContainerPlugin::class.java).forEach {
+            frameLayout.addView(it.view)
+            it.render()
+        }
         return this
-    }
-
-    private fun sendPlayBackViewToBack(playback: Playback) {
-        val playBackViewIndex = frameLayout.indexOfChild(playback.view)
-
-        var index = 0
-        while (index < playBackViewIndex)
-            frameLayout.getChildAt(index++).bringToFront()
     }
 }
