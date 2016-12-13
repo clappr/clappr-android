2f8c03532b42bee26ad22457fec2ebad8e6a6efe
Bruno Torres <me@brunotorr.es>
2016-12-13 21:22:00 -0200

    fix(base): remove external plugins/playbacks for now due to java incompatibilities

--- clappr/src/main/kotlin/io/clappr/player/base/Options.kt
+++ clappr/src/main/kotlin/io/clappr/player/base/Options.kt
@@ -1,14 +1,7 @@
 package io.clappr.player.base
 
-import io.clappr.player.components.Playback
-import io.clappr.player.plugin.Plugin
-import kotlin.reflect.KClass
-
 class Options(
     var source: String? = null,
     var mimeType: String? = null,
     var autoPlay: Boolean = true,
-    var plugins: List<KClass<Plugin>> = emptyList(),
-    var playbacks: List<KClass<Playback>> = emptyList(),
-    val options: Map<String, Any> = mutableMapOf<String, Any>()): Map<String, Any> by options {
-}
+    val options: Map<String, Any> = mutableMapOf<String, Any>()): Map<String, Any> by options
