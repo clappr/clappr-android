71baba0d6e172f270cad06e3ab6c4bedc71e2664
Bruno Torres <me@brunotorr.es>
2017-03-24 15:01:59 -0300

    chore(poster): remove unnecessary import

--- clappr/src/main/kotlin/io/clappr/player/plugin/PosterPlugin.kt
+++ clappr/src/main/kotlin/io/clappr/player/plugin/PosterPlugin.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.view.Gravity
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.jakewharton.picasso.OkHttp3Downloader
