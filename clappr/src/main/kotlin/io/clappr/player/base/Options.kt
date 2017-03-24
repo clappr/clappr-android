74d03ec73c51e37c68e9364d3feab134d721f002
Patrícia Muniz <pmunizferreira@gmail.com>
2017-03-24 17:57:11 -0300

    feat(options_update): change options from MutableMap to HashMap

--- clappr/src/main/kotlin/io/clappr/player/base/Options.kt
+++ clappr/src/main/kotlin/io/clappr/player/base/Options.kt
@@ -1,10 +1,12 @@
 package io.clappr.player.base
 
+import java.util.*
+
 class Options(
     var source: String? = null,
     var mimeType: String? = null,
     var autoPlay: Boolean = true,
-    val options: MutableMap<String, Any> = mutableMapOf<String, Any>()): MutableMap<String, Any> by options
+    val options: HashMap<String, Any> = hashMapOf<String, Any>()): MutableMap<String, Any> by options
 
 enum class ClapprOption(val value: String) {
     /**
