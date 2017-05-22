5ae4b1b1f4a379f5936fb067c74b9ba57eb56642
Amélia Pessoa <apessoa@thoughtworks.com>
2017-05-22 15:55:05 -0300

    feature(media_option): Support multiple audios and subtitles

--- clappr/src/main/kotlin/io/clappr/player/components/MediaOption.kt
+++ clappr/src/main/kotlin/io/clappr/player/components/MediaOption.kt
@@ -2,6 +2,8 @@ package io.clappr.player.components
 
 data class MediaOption(val name: String, val type: MediaOptionType, val raw: Any?, val info: Map<String, Any>?)
 
+val SUBTITLE_OFF = MediaOption("", MediaOptionType.SUBTITLE, null, null)
+
 enum class MediaOptionType {
     SUBTITLE,
     AUDIO
