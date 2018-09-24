0d983cee8712dd54185cb8ffd1a8ca3ae5c3c5c7
Arthur Zettler <arthur.zettler@gmail.com>
2018-09-24 10:47:04 -0300

    feat(time_indicator): register time indicator plugin

--- clappr/src/main/kotlin/io/clappr/player/plugin/PluginConfig.kt
+++ clappr/src/main/kotlin/io/clappr/player/plugin/PluginConfig.kt
@@ -1,6 +1,7 @@
 package io.clappr.player.plugin
 
 import io.clappr.player.plugin.Control.MediaControl
+import io.clappr.player.plugin.Control.TimeIndicatorPlugin
 
 object PluginConfig {
     fun register() {
@@ -8,5 +9,6 @@ object PluginConfig {
         Loader.registerPlugin(PosterPlugin::class)
         Loader.registerPlugin(LoadingPlugin::class)
         Loader.registerPlugin(MediaControl::class)
+        Loader.registerPlugin(TimeIndicatorPlugin::class)
     }
 }
