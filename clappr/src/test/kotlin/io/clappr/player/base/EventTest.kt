a08c3d8564277fa13f5786770fa71ab5083f4b49
Thomaz de Freitas Barbosa Cortez <thomazfcortez@gmail.com>
2018-11-14 16:41:59 -0300

    feat(rename_events): Tests improved

--- clappr/src/test/kotlin/io/clappr/player/base/EventTest.kt
+++ clappr/src/test/kotlin/io/clappr/player/base/EventTest.kt
@@ -1,17 +1,14 @@
 package io.clappr.player.base
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 import kotlin.test.assertEquals
 
-@RunWith(RobolectricTestRunner::class)
 class EventTest {
 
     @Test
     fun shouldHaveUniqueValue() {
         Event.values().forEach {
-            assertEquals(1, Event.values().filter { event -> event.value == it.value  }.size, "More than 1 event with \"${it.value}\" value")
+            assertEquals(1, Event.values().filter { event -> event.value == it.value }.size, "More than 1 event with \"${it.value}\" value")
         }
     }
 }
