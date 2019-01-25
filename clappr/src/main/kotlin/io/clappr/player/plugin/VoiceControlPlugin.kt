0ff78e23103f685641ed3c8e4c79fb2f14bfcbe4
Arthur Zettler <arthur.zettler@gmail.com>
2019-01-25 15:52:20 -0200

    refactor(voice_control): solve lint issues

--- clappr/src/main/kotlin/io/clappr/player/plugin/VoiceControlPlugin.kt
+++ clappr/src/main/kotlin/io/clappr/player/plugin/VoiceControlPlugin.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.util.Log
 import io.clappr.player.base.NamedType
 import io.clappr.player.components.Core
+import io.clappr.player.log.Logger
 import io.clappr.player.plugin.core.UICorePlugin
 
 class VoiceControlPlugin(core: Core) : UICorePlugin(core) {
@@ -34,77 +34,55 @@ class VoiceControlPlugin(core: Core) : UICorePlugin(core) {
 
         recognizeIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
 
-        recognizeIntent!!.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "pt-BR")
-        recognizeIntent!!.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, applicationContext.packageName)
-        recognizeIntent!!.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, MAX_RESULTS) // Optional
-        recognizeIntent!!.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, MINIMUM_LENGTH_MILLIS)
+        recognizeIntent?.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "pt-BR")
+        recognizeIntent?.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, applicationContext.packageName)
+        recognizeIntent?.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, MAX_RESULTS) // Optional
+        recognizeIntent?.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, MINIMUM_LENGTH_MILLIS)
 
-        recognizer!!.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(p0: Bundle?) {
-                Log.d("@@@", "onReadyForSpeech - $p0")
-            }
-
+        recognizer?.setRecognitionListener(object : RecognitionListener {
             override fun onRmsChanged(p0: Float) {}
+            override fun onReadyForSpeech(bundle: Bundle?) { Logger.debug(name, "onReadyForSpeech - $bundle") }
+            override fun onBufferReceived(buffer: ByteArray?) { Logger.debug(name, "onBufferReceived - $buffer") }
+            override fun onPartialResults(bundle: Bundle?) { Logger.debug(name, "onPartialResults - $bundle") }
+            override fun onEvent(event: Int, bundle: Bundle?) { Logger.debug(name, "onEvent - $bundle") }
+            override fun onBeginningOfSpeech() { Logger.debug(name, "onBeginningOfSpeech") }
+            override fun onEndOfSpeech() { Logger.debug(name, "onEndOfSpeech") }
 
-            override fun onBufferReceived(p0: ByteArray?) {
-                Log.d("@@@", "onBufferReceived - $p0")
-            }
+            override fun onResults(bundle: Bundle?) {
+                bundle?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.let { resultList ->
+                    Logger.debug(name, resultList.toString())
 
-            override fun onPartialResults(p0: Bundle?) {
-                Log.d("@@@", "onPartialResults - $p0")
-            }
-
-            override fun onEvent(p0: Int, p1: Bundle?) {
-                Log.d("@@@", "onEvent - $p0")
-            }
-
-            override fun onBeginningOfSpeech() {
-                Log.d("@@@", "onBeginningOfSpeech")
-            }
-
-            override fun onEndOfSpeech() {
-                Log.d("@@@", "onEndOfSpeech")
-            }
-
-            override fun onError(p0: Int) {
-                Log.d("@@@", "onError - ${getErrorText(p0)}")
-                recognizer?.startListening(recognizeIntent)
-            }
-
-            override fun onResults(command: Bundle?) {
-                Log.d("@@@", "onResults - $command")
-                var resultStringArrayList = command?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                Log.d("@@@", command?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION).toString())
-                recognizer?.startListening(recognizeIntent)
-                resultStringArrayList?.forEach {
-                    if (it!!.toUpperCase().contains("GLOBO")) {
-                        if (it!!.toUpperCase().contains("PAUSE")) {
-                            core.activePlayback?.pause()
-                        } else if (it!!.toUpperCase().contains("PLAY")) {
-                            core.activePlayback?.play()
-                        } else if (it!!.toUpperCase().contains("STOP")) {
-                            core.activePlayback?.stop()
+                    resultList.forEach {
+                        if (it.toUpperCase().contains("GLOBO")) {
+                            when {
+                                it.toUpperCase().contains("PAUSE") -> core.activePlayback?.pause()
+                                it.toUpperCase().contains("PLAY") -> core.activePlayback?.play()
+                                it.toUpperCase().contains("STOP") -> core.activePlayback?.stop()
+                            }
                         }
-                    } }
+                    }
+                }
+                
+                recognizer?.startListening(recognizeIntent)
             }
-          }
-        )
-      }
 
-    fun getErrorText(errorCode: Int): String {
-        val message: String
-        when (errorCode) {
-            SpeechRecognizer.ERROR_AUDIO -> message = "Audio recording error"
-            SpeechRecognizer.ERROR_CLIENT -> message = "Client side error"
-            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> message = "Insufficient permissions"
-            SpeechRecognizer.ERROR_NETWORK -> message = "Network error"
-            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> message = "Network timeout"
-            SpeechRecognizer.ERROR_NO_MATCH -> message = "No match"
-            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> message = "RecognitionService busy"
-            SpeechRecognizer.ERROR_SERVER -> message = "error from server"
-            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> message = "No speech input"
-            else -> message = "Didn't understand, please try again."
-        }
-        return message
+            override fun onError(errorCode: Int) {
+                Logger.debug(name, "onError - ${errorText(errorCode)}")
+                recognizer?.startListening(recognizeIntent)
+            }
+        })
+    }
+
+    private fun errorText(errorCode: Int) = when (errorCode) {
+        SpeechRecognizer.ERROR_AUDIO -> "Audio recording error"
+        SpeechRecognizer.ERROR_CLIENT -> "Client side error"
+        SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Insufficient permissions"
+        SpeechRecognizer.ERROR_NETWORK -> "Network error"
+        SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "Network timeout"
+        SpeechRecognizer.ERROR_NO_MATCH -> "No match"
+        SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RecognitionService busy"
+        SpeechRecognizer.ERROR_SERVER -> "error from server"
+        SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "No speech input"
+        else -> "Didn't understand, please try again."
     }
 }
