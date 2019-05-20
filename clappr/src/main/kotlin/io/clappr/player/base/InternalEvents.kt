011cf711be6146a00a444ee32fbd06542e54c7ac
uelitonfreitas <freitas.ueliton@gmail.com>
2019-05-20 14:17:08 -0300

    refactor(internal_events): remove uncessary keep

--- clappr/src/main/kotlin/io/clappr/player/base/InternalEvents.kt
+++ clappr/src/main/kotlin/io/clappr/player/base/InternalEvents.kt
@@ -1,7 +1,5 @@
 package io.clappr.player.base
 
-import android.support.annotation.Keep
-
 enum class InternalEvent (val value: String) {
     WILL_CHANGE_ACTIVE_CONTAINER("willChangeActiveContainer"),
     DID_CHANGE_ACTIVE_CONTAINER("didChangeActiveContainer"),
@@ -43,7 +41,6 @@ enum class InternalEvent (val value: String) {
 /**
  * Event bundle data keys for selected Events
  */
-@Keep
 enum class InternalEventData(val value: String) {
     HEIGHT("height"),
     WIDTH("width"),
