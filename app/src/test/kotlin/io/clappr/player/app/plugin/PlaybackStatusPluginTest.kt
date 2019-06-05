2a253a398b59c40fb17bd67fceb1245f092129dd
Arthur Zettler <arthur.zettler@gmail.com>
2019-06-05 18:38:18 -0300

    test(sample): update tests with roboletric 4.3

--- app/src/test/kotlin/io/clappr/player/app/plugin/PlaybackStatusPluginTest.kt
+++ app/src/test/kotlin/io/clappr/player/app/plugin/PlaybackStatusPluginTest.kt
@@ -2,27 +2,25 @@ package io.clappr.player.app.plugin
 
 import android.app.Application
 import android.widget.TextView
-import io.clappr.player.BuildConfig
+import androidx.test.core.app.ApplicationProvider
 import io.clappr.player.app.R
-import io.clappr.player.app.plugin.util.FakePlayback
 import io.clappr.player.app.plugin.assertPlugin.assertUiPluginHidden
 import io.clappr.player.app.plugin.assertPlugin.assertUiPluginShown
+import io.clappr.player.app.plugin.util.FakePlayback
 import io.clappr.player.base.BaseObject
 import io.clappr.player.base.Event
 import io.clappr.player.base.Options
 import io.clappr.player.components.Container
-import io.clappr.player.plugin.Loader
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import org.robolectric.shadows.ShadowApplication
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 @RunWith(RobolectricTestRunner::class)
-@Config(constants = BuildConfig::class, sdk = [23], application = Application::class)
+@Config(sdk = [23], application = Application::class)
 class PlaybackStatusPluginTest {
 
     private lateinit var container: Container
@@ -33,7 +31,7 @@ class PlaybackStatusPluginTest {
 
     @Before
     fun setup() {
-        BaseObject.applicationContext = ShadowApplication.getInstance().applicationContext
+        BaseObject.applicationContext = ApplicationProvider.getApplicationContext()
 
         container = Container(Options(source = source))
 
