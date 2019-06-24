ed72980dda04040d075f5affcb075d6f01d680d2
uelitonfreitas <freitas.ueliton@gmail.com>
2019-06-24 18:11:09 -0300

    refactor(clock): remove uncessary clock

--- clappr/src/main/kotlin/io/clappr/player/clocks/MonotonicClock.kt
+++
@@ -1,7 +0,0 @@
-package io.clappr.player.clocks
-
-import android.os.SystemClock
-
-typealias MonotonicClock = () -> Long
-
-val clapprMonotonicClock: MonotonicClock = { SystemClock.uptimeMillis() }
