416e34d4921e8554b22be65e3b518d0f4e57ea51
Lucas Pasquier <lalucaspasquier@Lucass-MacBook-Pro-3.local>
2019-07-12 11:33:50 -0300

    refactor: remove unnecessary classes

--- clappr/src/main/kotlin/io/clappr/player/shared/SharedData.kt
+++ clappr/src/main/kotlin/io/clappr/player/shared/SharedData.kt
@@ -1,23 +1,5 @@
 package io.clappr.player.shared
 
-import io.clappr.player.utils.MetricsSession
-
 class SharedData {
-    var metricsSession: MetricsSession
-
-    private val metricsSessionKey = "metricsSession"
-    private val storeMap  = mutableMapOf<String, Any>()
-
-    init {
-        metricsSession = createMetricsSessionIfNeeded()
-    }
-
-    private fun createMetricsSessionIfNeeded() =
-            storeMap[metricsSessionKey]?.let { it as MetricsSession } ?: setupMetrics()
-
-    private fun setupMetrics() : MetricsSession {
-        val metricsSession = MetricsSession()
-        storeMap[metricsSessionKey] = metricsSession
-        return metricsSession
-    }
+    val storeMap  = mutableMapOf<String, Any>()
 }
