3a0033a8517aa33c05299df51220d1da94d3d9b0
uelitonfreitas <freitas.ueliton@gmail.com>
2019-07-16 11:25:48 -0300

    feat: extract ExternalInputPlugin to an abstration

--- clappr/src/main/kotlin/io/clappr/player/plugin/core/externalinput/ExternalInputPlugin.kt
+++ clappr/src/main/kotlin/io/clappr/player/plugin/core/externalinput/ExternalInputPlugin.kt
@@ -1,14 +1,20 @@
 package io.clappr.player.plugin.core.externalinput
 
+import android.view.KeyEvent
 import io.clappr.player.base.NamedType
 import io.clappr.player.components.Core
+import io.clappr.player.log.Logger
 import io.clappr.player.plugin.PluginEntry
 import io.clappr.player.plugin.core.CorePlugin
 
-class ExternalInputPlugin(core: Core) : CorePlugin(core, name = name) {
+class ExternalInputPlugin(core: Core) : CorePlugin(core, name = name), ExternalInputDevice {
     companion object : NamedType {
         override val name = "externalInputPlugin"
 
         val entry = PluginEntry.Core(name = name, factory = { core -> ExternalInputPlugin(core) })
+    }
+
+    override fun holdKeyEvent(keyCode: Int, event: KeyEvent) {
+        Logger.debug(name, "Keycode: $keyCode, Event: $event")
     }
 }
