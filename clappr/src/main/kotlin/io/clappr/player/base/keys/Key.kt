adcbeecb6c6967ef33e26985d7c9d514a53ec56d
uelitonfreitas <freitas.ueliton@gmail.com>
2019-07-17 11:33:00 -0300

    broadcast PLAY_PAUSE key in core when pressed

--- clappr/src/main/kotlin/io/clappr/player/base/keys/Key.kt
+++ clappr/src/main/kotlin/io/clappr/player/base/keys/Key.kt
@@ -4,7 +4,8 @@ enum class Key(val value: String) {
     UNDEFINED("undefined"),
     PLAY("play"),
     PAUSE("pause"),
-    STOP("strop");
+    STOP("strop"),
+    PLAY_PAUSE("playPause");
 
     companion object {
         fun getByValue(value: String) = values().firstOrNull { it.value == value }?.let { it }
