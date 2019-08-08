825b3f0b7a4a2f0ca97d788ca6a5c7265daac610
Eduarda Barbosa <mequeiroz@thoughtworks.com>
2019-08-08 18:07:19 -0300

    refactor: optimize screen orientation changes event

--- clappr/src/main/kotlin/io/clappr/player/components/OrientationChangeListener.kt
+++ clappr/src/main/kotlin/io/clappr/player/components/OrientationChangeListener.kt
@@ -10,29 +10,40 @@ import io.clappr.player.base.EventData.ORIENTATION
 class OrientationChangeListener(private val core: Core) :
     OrientationEventListener(applicationContext, SENSOR_DELAY_NORMAL) {
 
-    var initialized = false
+    private var lastOrientation: Orientation? = null
 
     override fun onOrientationChanged(degrees: Int) {
-        if (!initialized) {
-            initialized = true
+        val orientation = Orientation.from(degrees)
+        if (hasNotChanged(orientation)) {
+            orientation?.let { lastOrientation = it }
             return
         }
 
-        val bundle =
-            Bundle().apply { putSerializable(ORIENTATION.value, Orientation.from(degrees)) }
+        orientation?.let { lastOrientation = it }
+
+        val bundle = Bundle().apply { putSerializable(ORIENTATION.value, orientation) }
 
         core.trigger(DID_CHANGE_SCREEN_ORIENTATION.value, bundle)
+    }
+
+    private fun hasNotChanged(orientation: Orientation?) =
+        orientation == null || lastOrientation == null || lastOrientation == orientation
+
+    override fun enable() {
+        if (canDetectOrientation()) {
+            super.enable()
+        }
     }
 }
 
 enum class Orientation {
-    PORTRAIT, LANDSCAPE, UNKNOWN;
+    PORTRAIT, LANDSCAPE;
 
     companion object {
         fun from(orientation: Int) = when (orientation) {
             0, 180 -> PORTRAIT
             90, 270 -> LANDSCAPE
-            else -> UNKNOWN
+            else -> null
         }
     }
 }
--- clappr/src/test/kotlin/io/clappr/player/components/OrientationChangeListenerTest.kt
+++ clappr/src/test/kotlin/io/clappr/player/components/OrientationChangeListenerTest.kt
@@ -5,8 +5,8 @@ import androidx.test.core.app.ApplicationProvider
 import io.clappr.player.base.BaseObject
 import io.clappr.player.base.Event
 import io.clappr.player.base.Options
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
+import org.hamcrest.CoreMatchers.hasItems
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -45,7 +45,7 @@ class OrientationChangeListenerTest {
             orientation = it?.get("orientation") as Orientation?
         }
 
-        val orientationChangeListener = orientationChangeListenerInitialized(core)
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 90)
         orientationChangeListener.onOrientationChanged(0)
 
         assertEquals(Orientation.PORTRAIT, orientation)
@@ -61,7 +61,7 @@ class OrientationChangeListenerTest {
             orientation = it?.get("orientation") as Orientation?
         }
 
-        val orientationChangeListener = orientationChangeListenerInitialized(core)
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 0)
         orientationChangeListener.onOrientationChanged(90)
 
         assertEquals(Orientation.LANDSCAPE, orientation)
@@ -77,7 +77,7 @@ class OrientationChangeListenerTest {
             orientation = it?.get("orientation") as Orientation?
         }
 
-        val orientationChangeListener = orientationChangeListenerInitialized(core)
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 90)
         orientationChangeListener.onOrientationChanged(180)
 
         assertEquals(Orientation.PORTRAIT, orientation)
@@ -93,30 +93,89 @@ class OrientationChangeListenerTest {
             orientation = it?.get("orientation") as Orientation?
         }
 
-        val orientationChangeListener = orientationChangeListenerInitialized(core)
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 0)
         orientationChangeListener.onOrientationChanged(270)
 
         assertEquals(Orientation.LANDSCAPE, orientation)
     }
 
     @Test
-    fun `should trigger DID_CHANGE_SCREEN_ORIENTATION with UNKNOWN when orientation cannot be determined`() {
+    fun `should trigger DID_CHANGE_SCREEN_ORIENTATION when orientation changes multiple times`() {
         val core = Core(Options())
 
-        var orientation: Orientation? = null
+        val orientations: MutableList<Orientation?> = mutableListOf()
 
+        core.on(Event.DID_CHANGE_SCREEN_ORIENTATION.value) {
+            orientations += it?.get("orientation") as Orientation?
+        }
+
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 0)
+        orientationChangeListener.onOrientationChanged(270)
+        orientationChangeListener.onOrientationChanged(0)
+
+        assertThat(orientations, hasItems(Orientation.LANDSCAPE, Orientation.PORTRAIT))
+
+    }
+
+    @Test
+    fun `should not trigger DID_CHANGE_SCREEN_ORIENTATION when orientation does not change`() {
+        val core = Core(Options())
+
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 270)
+
+        var orientation: Orientation? = null
         core.on(Event.DID_CHANGE_SCREEN_ORIENTATION.value) {
             orientation = it?.get("orientation") as Orientation?
         }
 
-        val orientationChangeListener = orientationChangeListenerInitialized(core)
-        orientationChangeListener.onOrientationChanged(ORIENTATION_UNKNOWN)
+        orientationChangeListener.onOrientationChanged(270)
 
-        assertEquals(Orientation.UNKNOWN, orientation)
+
+        assertNull(orientation)
     }
 
-    private fun orientationChangeListenerInitialized(core: Core): OrientationChangeListener =
+    @Test
+    fun `should not trigger DID_CHANGE_SCREEN_ORIENTATION when orientation goes from something to unknown and back to the same`() {
+        val core = Core(Options())
+
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 270)
+
+        var orientation: Orientation? = null
+        core.on(Event.DID_CHANGE_SCREEN_ORIENTATION.value) {
+            orientation = it?.get("orientation") as Orientation?
+        }
+
+        orientationChangeListener.onOrientationChanged(ORIENTATION_UNKNOWN)
+        orientationChangeListener.onOrientationChanged(270)
+
+
+        assertNull(orientation)
+    }
+
+    @Test
+    fun `should trigger DID_CHANGE_SCREEN_ORIENTATION when orientation changes while in unknown state`() {
+        val core = Core(Options())
+
+        val orientationChangeListener = orientationChangeListenerInitialized(core, 270)
+
+        var orientation: Orientation? = null
+        core.on(Event.DID_CHANGE_SCREEN_ORIENTATION.value) {
+            orientation = it?.get("orientation") as Orientation?
+        }
+
+        orientationChangeListener.onOrientationChanged(ORIENTATION_UNKNOWN)
+        orientationChangeListener.onOrientationChanged(0)
+
+
+        assertEquals(Orientation.PORTRAIT, orientation)
+    }
+
+
+    private fun orientationChangeListenerInitialized(
+        core: Core,
+        initialDegrees: Int
+    ): OrientationChangeListener =
         OrientationChangeListener(core).apply {
-            onOrientationChanged(0)
+            onOrientationChanged(initialDegrees)
         }
 }
