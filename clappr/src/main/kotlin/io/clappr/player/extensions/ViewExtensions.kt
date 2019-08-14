5f2d19857bce30de7f1fc374a4d60796bc1538dc
ycouto <ycouto@thoughtworks.com>
2019-08-14 11:42:14 -0300

    feat: simplifying animation extension on view

--- clappr/src/main/kotlin/io/clappr/player/extensions/ViewExtensions.kt
+++ clappr/src/main/kotlin/io/clappr/player/extensions/ViewExtensions.kt
@@ -1,13 +1,8 @@
 package io.clappr.player.extensions
 
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-
-fun View.removeFromParent() {
-    (this.parent as? ViewGroup)?.removeView(this)
-}
 
 fun View.animate(res: Int, onAnimationEnd: () -> Unit = {}): Animation {
     val animator = AnimationUtils.loadAnimation(context, res)
@@ -17,21 +12,10 @@ fun View.animate(res: Int, onAnimationEnd: () -> Unit = {}): Animation {
         }
 
         override fun onAnimationEnd(animation: Animation?) {
-            if (this@animate is ViewGroup)
-                this@animate.enableAllViews()
-            else {
-                isEnabled = true
-            }
             onAnimationEnd()
-
         }
 
         override fun onAnimationStart(animation: Animation?) {
-            if (this@animate is ViewGroup)
-                this@animate.disableAllViews()
-            else {
-                isEnabled = false
-            }
         }
     })
 
