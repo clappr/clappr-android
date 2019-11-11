351c1f8db4a384c28e6f19ccc7fe79650f7cfddf
Arthur Zettler <arthur.zettler@gmail.com>
2019-11-11 17:51:08 -0300

    test(bundle_extension): ensure is long press flag is translated to input key correctly

--- clappr/src/test/kotlin/io/clappr/player/extensions/BundleExtensionsTest.kt
+++ clappr/src/test/kotlin/io/clappr/player/extensions/BundleExtensionsTest.kt
@@ -8,25 +8,44 @@ import io.mockk.every
 import io.mockk.mockk
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class BundleExtensionsTest {
     @Test
     fun `should extract key and action from bundle`() {
-        val bundle = mockk<Bundle>()
+        val bundle = mockk<Bundle>(relaxed = true)
 
         every { bundle.getString(EventData.INPUT_KEY_CODE.value) } returns Key.BACK.value
         every { bundle.getString(EventData.INPUT_KEY_ACTION.value) } returns Action.UP.value
 
         val inputKey = bundle.extractInputKey()
 
-        assertEquals(Key.BACK, inputKey?.key)
-        assertEquals(Action.UP, inputKey?.action)
+        assertNotNull(inputKey)
+        assertEquals(Key.BACK, inputKey.key)
+        assertEquals(Action.UP, inputKey.action)
+    }
+
+    @Test
+    fun `should extract is long press flag from bundle`() {
+        val bundle = mockk<Bundle>()
+
+        every { bundle.getString(EventData.INPUT_KEY_CODE.value) } returns Key.BACK.value
+        every { bundle.getString(EventData.INPUT_KEY_ACTION.value) } returns Action.UP.value
+        every {
+            bundle.getBoolean(EventData.INPUT_KEY_IS_LONG_PRESS.value, false)
+        } returns true
+
+        val inputKey = bundle.extractInputKey()
+
+        assertNotNull(inputKey)
+        assertTrue(inputKey.isLongPress, "Is long press should be true")
     }
 
     @Test
     fun `should return null when there is no key available`() {
-        val bundle = mockk<Bundle>()
+        val bundle = mockk<Bundle>(relaxed = true)
 
         every { bundle.getString(EventData.INPUT_KEY_CODE.value) } returns null
         every { bundle.getString(EventData.INPUT_KEY_ACTION.value) } returns Action.UP.value
@@ -38,7 +57,7 @@ class BundleExtensionsTest {
 
     @Test
     fun `should return null when there is no action available`() {
-        val bundle = mockk<Bundle>()
+        val bundle = mockk<Bundle>(relaxed = true)
 
         every { bundle.getString(EventData.INPUT_KEY_CODE.value) } returns Key.BACK.value
         every { bundle.getString(EventData.INPUT_KEY_ACTION.value) } returns null
