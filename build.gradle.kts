212985b0d94e982512699501e80814de8d8f9860
Túlio Hoffimann Mendes <tulio.hoffimann@thoughtworks.com>
2019-11-20 14:05:09 -0300

    refactor(kotlin_gradle_dsl): migrate some groovy build scripts to kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.50'
-
     repositories {
         mavenCentral()
         mavenLocal()
@@ -11,10 +9,10 @@ buildscript {
     }
 
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.1'
-        classpath 'net.saliman:gradle-cobertura-plugin:2.2.5'
-        classpath 'org.kt3k.gradle.plugin:coveralls-gradle-plugin:2.3.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.5.1")
+        classpath("net.saliman:gradle-cobertura-plugin:2.2.5")
+        classpath("org.kt3k.gradle.plugin:coveralls-gradle-plugin:2.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
     }
 }
 
