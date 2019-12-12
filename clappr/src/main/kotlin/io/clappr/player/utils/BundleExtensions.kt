4ee79e9949025a655aeeb0887cc2a70394cbc6fa
Ricardo Cavalcanti <rcaval@thoughtworks.com>
2019-12-12 17:59:23 -0300

    refactor: remove nullable form method signature as it is not necessary

--- clappr/src/main/kotlin/io/clappr/player/utils/BundleExtensions.kt
+++ clappr/src/main/kotlin/io/clappr/player/utils/BundleExtensions.kt
@@ -3,6 +3,6 @@ package io.clappr.player.utils
 import android.os.Bundle
 import java.io.Serializable
 
-fun Bundle.withPayload(vararg pairs: Pair<String, Serializable>): Bundle? = apply {
+fun Bundle.withPayload(vararg pairs: Pair<String, Serializable>): Bundle = apply {
     pairs.forEach { (key, value) -> putSerializable(key, value) }
 }
