d030222f709e8a4ccf4cc2126383584b7bbec661
Ricardo Cavalcanti <rcaval@thoughtworks.com>
2019-12-23 16:04:23 -0300

    fix: replace AnalyticsListener with EventLogger in order to avoid AbstractMethodError

--- clappr/src/main/kotlin/io/clappr/player/playback/VideoResolutionChangeListener.kt
+++ clappr/src/main/kotlin/io/clappr/player/playback/VideoResolutionChangeListener.kt
@@ -2,12 +2,13 @@ package io.clappr.player.playback
 
 import android.os.Bundle
 import com.google.android.exoplayer2.analytics.AnalyticsListener
+import com.google.android.exoplayer2.util.EventLogger
 import io.clappr.player.base.Event
 import io.clappr.player.base.EventData
 import io.clappr.player.components.Playback
 import io.clappr.player.utils.withPayload
 
-class VideoResolutionChangeListener(private val playback: Playback) : AnalyticsListener {
+class VideoResolutionChangeListener(private val playback: Playback) : EventLogger(null) {
 
     override fun onVideoSizeChanged(eventTime: AnalyticsListener.EventTime?, width: Int, height: Int, unappliedRotationDegrees: Int, pixelWidthHeightRatio: Float) {
         val userData = Bundle().withPayload(
